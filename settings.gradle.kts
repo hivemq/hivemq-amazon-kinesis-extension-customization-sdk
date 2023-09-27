fe88da3f59482ed44aa5d8ab2db92e854bdfc252
Silvio Giebl <silvio.giebl@hivemq.com>
2023-09-27 12:01:29 +0200

    Improve ordering in settings.gradle.kts (pluginManagement > plugins > script)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
-rootProject.name = "hivemq-amazon-kinesis-extension-customization-sdk"
-
 pluginManagement {
     if (file("../hivemq/plugins").exists()) {
         includeBuild("../hivemq/plugins")
     }
 }
+
+rootProject.name = "hivemq-amazon-kinesis-extension-customization-sdk"
 
 if (file("../hivemq-extension-sdk").exists()) {
     includeBuild("../hivemq-extension-sdk")
